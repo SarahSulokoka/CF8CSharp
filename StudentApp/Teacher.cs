5d3c8f3560c506211e1fba694be4429d6b98c789
Sara <sarahsulokoka@gmail.com>
2025-09-15 19:42:04 +0300

    Student updated

--- StudentApp/Teacher.cs
+++ StudentApp/Teacher.cs
@@ -8,5 +8,38 @@ namespace StudentApp
 {
     internal class Teacher
     {
+        public int Id { get; set; } // Added private setter auto implemented property
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id} {Firstname} {Lastname}";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        //public override string ToString() => return $"{Id} {Firstname} {Lastname}";
+
+        //Do we need a constructor for the override?
+        //No, we don't need a constructor for the override.
+        //When do we need a constructor?
+        //We need a constructor when we want to initialize the object with specific values.
+
+        public Teacher() //default constructor
+        {
+        }
+
+
+
     }
+
 }
