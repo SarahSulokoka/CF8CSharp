a88597da8b27b88ae0ebd52d3e94f5524ef29d67
Sara <sarahsulokoka@gmail.com>
2025-09-16 22:40:06 +0300

    Dependancy Injection App

---
+++ DependancyInjection/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DependancyInjection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
