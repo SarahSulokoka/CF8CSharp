db99f5b94d1761b46e56ae303c40ec6c7e9ae0e4
Sara <sarahsulokoka@gmail.com>
2025-09-16 22:33:11 +0300

    FractionApp added

--- PointApp/Model/Point.cs
+++ PointApp/Model/Point.cs
@@ -12,7 +12,20 @@ namespace PointApp.Model
 
         public override string? ToString()
         {
-            return base.ToString();
+            return $"{X}";
+        }
+
+        public void Move5()
+        {
+            X += 5;
+        }
+
+    
+
+        public override void Move5()
+        {
+            base.Move5(); // Call the base class method to move X by 5
+            Y += 5;
         }
     }
 }
--- PointApp/Model/Point2D.cs
+++ PointApp/Model/Point2D.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace PointApp.Model
 {
-    internal class Point2D
+    internal class Point2D : Point
     {
+        public int Y { get; set; }
+
+        //public Point2D() : base() 
+        //{ 
+        //    Y = 0;
+        //}
+
+        public override string? ToString()
+        {
+            return $"({X}, {Y})";
+        }
+
+        public override void Move5()
+        {
+            base.Move5();
+            Y += 5;
+        }
     }
 }
