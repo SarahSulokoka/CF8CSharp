db95589fd39b2b2849982265a8e5dfe2311595e7
Sara <sarahsulokoka@gmail.com>
2025-09-16 23:10:38 +0300

    InterfaceApp Added

---
+++ AbstractClassApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AbstractClassApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Interfaces/AbstractPoint.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    internal class AbstractPoint : IMoveable
+
+    {
+        public  int X { get; set; }
+        public virtual void Move5()
+        {
+            X += 5;
+        }
+        public virtual void Move6() {
+            X += 6;
+        }
+}
