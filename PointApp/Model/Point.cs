c8c08100780626bf61e18b96e9d29fd93f02501b
Sara <sarahsulokoka@gmail.com>
2025-09-16 21:56:55 +0300

    Point App added

---
+++ PointApp/Model/Point.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PointApp.Model
+{
+    internal class Point
+    {
+        public int X { get; set; }
+
+        public override string? ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
