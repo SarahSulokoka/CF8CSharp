d0df85752503165948fe09ea03fa57c9b36b1433
Sara <sarahsulokoka@gmail.com>
2025-09-17 19:05:42 +0300

    Product Model Added to Collections

---
+++ CollectionsApp/Model/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsApp.Model
+{
+
+    //sorting in Collections 
+    internal class Product : IEquatable<Product> , IComparable<Product>
+
+    {
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+
+        public int CompareTo(Product? other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Equals(Product? other)    // better performance, contains uses it . also in Dictionary
+     
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
+    }
+}
